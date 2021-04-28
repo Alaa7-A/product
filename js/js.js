fdca870fab0899d12b931a62d4ea592546514b70
alaaalzhoury <alaaalzhore3@gmail.com>
2021-04-28 18:45:48 +0300

    s

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+    if (myVideo.paused) 
+        myVideo.play();
+    else 
+        myVideo.pause(); 
+}
+$(document).ready(function() {
+    $(function () {
+        $("#commentForm").validate();
+    });
+});
